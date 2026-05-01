6800979ece18563df3d1d3b3ca8c8fed184c203c
Dan <dnadna59@gmail.com>
2026-05-01 10:23:11 +0200

    set DOCKER_DEFAULT_PLATFORM=linux/amd64 for aarch64 runner
    archlinux:latest has no arm64 image; force amd64 for all kernel builder
    containers the same way build_image.sh does.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # shellcheck source=lib/build-common.sh
 source "$SCRIPT_DIR/lib/build-common.sh"
